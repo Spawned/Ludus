2a7bca03a75ad977cefee89bb177e7026ce165cf
OscarOrSomething <oscar.shrimpton.personal@gmail.com>
2015-03-06 18:42:24 +0000

    Fixed missing descriptions for some @return/@param statements

--- src/main/java/in/spawned/ludus/interfaces/Arena.java
+++ src/main/java/in/spawned/ludus/interfaces/Arena.java
@@ -21,52 +21,47 @@ public interface Arena {
 
     /**
      * Returns the reason the given player cannot join or null.
-     * @param p
-     * @return
+     * @param p The player requesting to join
+     * @return An optional object returning the reason the player can't join or nothing
      */
     public Optional<String> canJoin(Player p);
 
     /**
      * Adds the player specified to the arena: This should not be called before checking the player can join.
-     *
-     * @param p
-     * @return
+     * @param p The player to add
      */
     public void addPlayer(Player p);
 
     /**
      * Removes the given player from this arena: Check the player is in the arena first.
-     *
-     * @param p
-     * @return
+     * @param p The player to remove
      */
     public void removePlayer(Player p);
 
     /**
      * Returns the current session in this arena.
-     *
-     * @return
+     * @return The current Session
+     * @see in.spawned.ludus.interfaces.Session
      */
     public Session getSession();
 
     /**
      * Gets the players currently in this arena.
-     * @return
+     * @return The players currently in this arena
      */
     public Player[] getPlayers();
 
     /**
      * Sets the session in this arena. Should call session.destroy
-     *
-     * @param s
+     * @param s The new session
      */
     public void setSession(Session s);
 
     /**
      * Returns whether or not the given player is in this arena.
      *
-     * @param p
-     * @return
+     * @param p The player to check for
+     * @return Whether or not the player is in this arena
      */
     public boolean hasPlayer(Player p);
 
